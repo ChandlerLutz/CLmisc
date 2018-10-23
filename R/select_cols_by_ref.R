c7b540219a106e0c86e5fb7ca50590a7db3ced4e
Chandler Lutz <chandler.lutz@gmail.com>
2018-10-23 16:54:13 -0400

    updated selection by reference

--- R/select_cols_by_ref.R
+++ R/select_cols_by_ref.R
@@ -4,13 +4,19 @@
 ##    Questions/comments: cl.eco@cbs.dk
 ##    $Revisions:      1.0.0     $Date:  2018-10-23
 
-#' Keep columns in a data.table by reference
+#' Select columns in a data.table by reference
 #'
 #' @param DT a \code{data.table}
 #' @param keep.cols a character vector of columns in \code{DT}
+#' @examples
+#' data(mtcars)
+#' DT <- as.data.table(mtcars)
+#' DT <- select_by_ref(DT, "hp")
+#' head(DT)
+#'
 #' @export
-keep_cols_by_ref <- function(DT, keep.cols) {
-  if (!is.character(cols)) stop("Error: keep.cols must be a character vector")
+select_by_ref <- function(DT, keep.cols) {
+  if (!is.character(keep.cols)) stop("Error: keep.cols must be a character vector")
   delete.cols <- names(DT) %>% .[!(. %chin% keep.cols)]
   DT <- DT[, c(delete.cols) := NULL]
   return(DT)
