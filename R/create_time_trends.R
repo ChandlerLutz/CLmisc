59af9b0e81baf359841602db6e437fa0b75088e6
Chandler Lutz <chandler.lutz@gmail.com>
2019-05-13 18:36:06 -0400

    change argument name for create_time_trends

--- R/create_time_trends.R
+++ R/create_time_trends.R
@@ -18,7 +18,7 @@
 #' @param .data a \code{data.table}
 #' @param time.col a time column; can be of any type. `time.col` will
 #'   be ordered using \code{order()}
-#' @param char.col a \code{character} column that will be used to create
+#' @param select.column a column that will be used to create
 #' @param quadratic.time.trends a \code{logical} for whether to include
 #'   quadratic (squared) time trends __IN ADDITION__ to the linear
 #'   time trends for the the dummy columns. Default is \code{FALSE}
@@ -27,8 +27,8 @@
 #'   models. Default is \code{FALSE}
 #' @return A data.table with the same number of rows, but the time
 #'   trends included. The format of the time trend columns will be
-#'   \code{tt.char.col_value} (linear trends) or \code{tt2.char.col_value}
-#'   (quadratic trends) where \code{char.col} is the name of the character
+#'   \code{tt.select.column_value} (linear trends) or \code{tt2.select.column_value}
+#'   (quadratic trends) where \code{select.column} is the name of the character
 #'   column requested from the user and \code{value} is the value of the of
 #'   the character column for that time trend
 #' @export
@@ -38,25 +38,25 @@
 #'                crime = 1:3, stringsAsFactors = FALSE))
 #' crime2 <- copy(crime)
 #' print(create_time_trends(crime2, time.col = "year",
-#'                          char.col = "city"))
-create_time_trends <- function(.data, time.col, char.col,
+#'                          select.column = "city"))
+create_time_trends <- function(.data, time.col, select.column,
                                quadratic.time.trends = FALSE,
                                remove.first.trend = FALSE) {
 
   ##some data checks
   stopifnot(is.data.table(.data),
             is.character(time.col),
-            is.character(char.col)
+            is.character(select.column)
             )
 
   ##for R cmd check
   temp.time.col <- temp.time.trend <- temp.time.trend2 <- NULL
-  
+
   .data <- .data[, temp.time.col := get(time.col)]
 
   ##the time trends
   .data.tt <- .data %>%
-    .[, .(temp.time.col = unique(temp.time.col))] %>% 
+    .[, .(temp.time.col = unique(temp.time.col))] %>%
     .[order(temp.time.col)] %>%
     ##linear time trend
     .[, temp.time.trend := 1:.N] %>%
@@ -66,23 +66,23 @@ create_time_trends <- function(.data, time.col, char.col,
   ##Merge in the time trends
   .data <- .data %>%
     merge(.data.tt, by = "temp.time.col") %>%
-    dummy_cols(select_columns = char.col, remove_first_dummy = remove.first.trend)
+    dummy_cols(select_columns = select.column, remove_first_dummy = remove.first.trend)
 
   ## -- Linear time trends -- ##
 
   ##the names of the dummy variables
-  names.dummies <- names(.data) %>% .[grepl(paste0("^", char.col, "_"), x= .)]
+  names.dummies <- names(.data) %>% .[grepl(paste0("^", select.column, "_"), x= .)]
   ##updated names for the time trends
   names.dummies.tt <- paste0("tt.", names.dummies)
-  
+
   .data <- .data %>%
     ##update the names
-    data.table::setnames(names.dummies, names.dummies.tt) %>% 
+    data.table::setnames(names.dummies, names.dummies.tt) %>%
     ## multiply the dummies by the time trend
     .[, c(names.dummies.tt) := lapply(.SD, function(x) x * temp.time.trend),
       .SDcols = names.dummies.tt]
 
-  print(sprintf("The regex for the linear time trend columns are `^tt.%s_`", char.col))
+  print(sprintf("The regex for the linear time trend columns are `^tt.%s_`", select.column))
 
   ## -- Quadratic time trends if requested by the user -- ##
 
@@ -90,9 +90,9 @@ create_time_trends <- function(.data, time.col, char.col,
     ##user requested linear time trends
 
     .data <- .data %>%
-      dummy_cols(select_columns = char.col, remove_first_dummy = remove.first.trend)
+      dummy_cols(select_columns = select.column, remove_first_dummy = remove.first.trend)
     ##the names of the dummy variables
-    names.dummies <- names(.data) %>% .[grepl(paste0("^", char.col, "_"))]
+    names.dummies <- names(.data) %>% .[grepl(paste0("^", select.column, "_"))]
     ##updated names for the time trends
     names.dummies.tt2 <- paste0("tt2.", names.dummies)
 
@@ -105,13 +105,13 @@ create_time_trends <- function(.data, time.col, char.col,
 
     ##print the regex
     print(sprintf("The regex for the linear time trend columns are `^tt.%s_`",
-                  char.col))
-    
+                  select.column))
+
     ##clean up
     .data <- .data[, c("temp.time.trend2")]
 
   }
-  
+
   ##clean up
   .data <- .data[, c("temp.time.col", "temp.time.trend") := NULL]
 
