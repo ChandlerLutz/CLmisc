01339cf4ee9d8f52701e5db930cd52fe09cbe807
Chandler Lutz <chandler.lutz@gmail.com>
2020-07-15 07:55:03 -0700

    add cut_with_bin_labels

---
+++ R/cut_with_bin_labels.R
@@ -0,0 +1,71 @@
+## c:/Dropbox/Rpackages/CLmisc/R/cut_with_bin_labels.R
+
+##    Chandler Lutz
+##    Questions/comments: cl.eco@cbs.dk
+##    $Revisions:      1.0.0     $Date:  2020-07-15
+
+#' `cut` using endpoints as bin labels bin labels
+#'
+#' Use endpoints as bin labels with `cut`. The only difference between
+#' this function and `cut`, is that this function will label the
+#' levels based on the endpoints if `labels` are specified as either
+#' "left" or "right". For more information, see `cut`
+#'
+#' @seealso `cut`
+#'
+#' @param x a numeric vector which is to be converted to a factor by
+#'   cutting.
+#' @param breaks either a numeric vector of two or more unique cut
+#'   points or a single number (greater than or equal to 2) giving the
+#'   number of intervals into which 'x' is to be cut.
+#' @param labels labels for the levels of the resulting category.  By
+#'   default, labels are constructed using '"(a,b]"' interval
+#'   notation.  If 'labels = FALSE', simple integer codes are returned
+#'   instead of a factor. If 'labels = "left"' then the labels will be
+#'   a numeric verctor with the left endpoints. If 'labels = "right"'
+#'   then the labels will be a numeric verctor with the right
+#'   endpoints.
+#' @param include.lowest logical, indicating if an 'x[i]' equal to the
+#'   lowest (or highest, for 'right = FALSE') 'breaks' value should be
+#'   included.
+#' @param right logical, indicating if the intervals should be closed
+#'   on the right (and open on the left) or vice versa. Default is `TRUE`
+#' @param dig.lab integer which is used when labels are not given.  It
+#'   determines the number of digits used in formatting the break
+#'   numbers.
+#' @param order_result logical: should the result be an ordered
+#'   factor?
+#' @param ... further arguments passed to or from other methods.
+#' @export
+#' @examples
+#' data(mtcars)
+#' breaks <- seq(from = 0, to = max(mtcars$hp) + 10, by = 10)
+#' data.table(hp = mtcars$hp,
+#'           bins.left.endpoint = cut_with_bin_labels(mtcars$hp, breaks = breaks, labels = "left"),
+#'           bins.right.endpoint = cut_with_bin_labels(mtcars$hp, breaks = breaks, labels = "right")
+#'           ) %>% print
+cut_with_bin_labels <- function(x, breaks, labels = NULL,
+         include.lowest = FALSE, right = TRUE, dig.lab = 3,
+         ordered_result = FALSE, ...) {
+
+  if (!is.null(labels) && labels %in% c("right", "left")) {
+    out <- cut(x = x, breaks = breaks,
+               include.lowest = include.lowest,
+               right = right, dig.lab = dig.lab,
+               ordered_result = ordered_result, ...) %>%
+      as.character
+    if (labels == "left") {
+      out <- gsub("\\((.*),.*\\]$", "\\1", x = out) %>%
+        as.numeric
+    } else if (labels == "right") {
+      out <- gsub("\\(.*,(.*)\\]$", "\\1", x = out) %>%
+        as.numeric
+    }
+  } else {
+    out <- cut(x = x, breaks = breaks, labels = labels,
+               include.lowest = include.lowest,
+               right = right, dig.lab = dig.lab,
+               ordered_result = ordered_result, ...)
+  }
+  return(out)
+}
